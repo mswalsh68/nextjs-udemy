72c62ecec06de56b4e06832a02765c78286ba7ee
Mike Walsh <mswalsh68@gmail.com>
2025-11-21 14:46:56 -0500

    image slideshow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/footer/footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
