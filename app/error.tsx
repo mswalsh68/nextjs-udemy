fea706a4a1f12affa82b803b6962e7d9dba11fb6
Mike Walsh <mswalsh68@gmail.com>
2025-11-24 15:26:16 -0500

    error message

---
+++ app/error.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function Error() {
+  return (
+    <main className={'error'}>
+        <h1>
+            Oh, something broke.
+        </h1>
+        <p>
+            Please try again later.
+        </p>
+    </main>
+  )
+}
