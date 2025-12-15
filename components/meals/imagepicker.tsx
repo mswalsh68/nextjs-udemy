71c43bc6ba1f1cb8080edda7874ae104b1922289
Mike Walsh <mswalsh68@gmail.com>
2025-12-15 12:50:55 -0500

    image picker initial

--- components/meals/imagepicker.tsx
+++ components/meals/imagepicker.tsx
@@ -1,10 +1,31 @@
+'use client'
+import { useRef } from 'react';
 import classes from './imagepicker.module.css'
 
-export default function ImagePicker() {
+export default function ImagePicker({label, name}: {label: string; name: string}) {
+
+const imageInput = useRef<HTMLInputElement>(null);
+
+  function handleImagePick() {
+    imageInput.current?.click()
+  }
+
   return (
     <div className={classes.picker}>
-      <label htmlFor="image">label</label>
-      <input type="file" id="image" name="image" accept="image/*" />
+      <label htmlFor={name}>{label}</label>
+      <div>
+        <input 
+          className={classes.input}
+          type="file" 
+          id={name}
+          name={name} 
+          accept="image/png, image/jpeg" 
+          ref={imageInput}
+        />
+        <button className={classes.button} type="button" onClick={handleImagePick}>
+          Choose Image
+        </button>
+      </div>
     </div>
   )
 }
